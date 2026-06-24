c080137429b64175e110c1ae0cca1c6171699f50
RoboDoig <erskine94@gmail.com>
2026-06-24 14:51:54 +0100

    Implemented hardware interface layer

--- examples/rig.py
+++ examples/rig.py
@@ -3,8 +3,29 @@ import os
 from ucl_open_freeing_vision_benchmark.rig import (
     UclOpenFreeingVisionBenchmarkRig
 )
+from ucl_open.devices.behavior_board import BehaviorBoard, CameraTriggerController
+from ucl_open.video import SpinnakerCamera
 
-rig = UclOpenFreeingVisionBenchmarkRig()
+rig = UclOpenFreeingVisionBenchmarkRig(
+    root_path="../temp_data",
+    behavior_board=BehaviorBoard(
+        port_name="COM13",
+        camera_trigger_controller=CameraTriggerController(
+            trigger0_frequency=150,
+            trigger1_frequency=150
+        )
+    ),
+    track_camera=SpinnakerCamera(
+        serial_number="25224819",
+        trigger_frequency=150,
+        exposure_time=3000
+    ),
+    subject_camera=SpinnakerCamera(
+        serial_number="18474551",
+        trigger_frequency=150,
+        exposure_time=3000
+    ),
+)
 
 def main(path_seed: str = "./local/{schema}.json"):
     os.makedirs(os.path.dirname(path_seed), exist_ok=True)
--- src/ucl_open_freeing_vision_benchmark/rig.py
+++ src/ucl_open_freeing_vision_benchmark/rig.py
@@ -2,10 +2,14 @@ from typing import Literal, Dict
 from pydantic import Field
 
 from ucl_open.core.rig import Rig
+from ucl_open.devices.behavior_board import BehaviorBoard
+from ucl_open.video import SpinnakerCamera
 
 from ucl_open_freeing_vision_benchmark import __semver__
 
 
 class UclOpenFreeingVisionBenchmarkRig(Rig):
     version: Literal[__semver__] = __semver__
-    ...
+    behavior_board: BehaviorBoard
+    subject_camera: SpinnakerCamera
+    track_camera: SpinnakerCamera
